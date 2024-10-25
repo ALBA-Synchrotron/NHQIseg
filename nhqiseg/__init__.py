903091ddbe39652d6c7671efc343fb4da0e6178e
Roberto Javier Homs Puron <rhoms@cells.es>
2024-10-25 12:27:57 +0200

    Refactor project to use pyproject.toml and setuptool-scm

--- nhqiseg/__init__.py
+++ nhqiseg/__init__.py
@@ -1,7 +1,3 @@
 from .communication import NHQComm
 from .constant import Models, State
 from .controller import NHQPowerSupply
-
-# The version is updated automatically with bumpversion
-# Do not update manually
-version = '0.2.0'
